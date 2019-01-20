4e48e0e23fada07874feb27b026cbaf2a736f92a
“Pengyang <zhou_py@qq.com>
2019-01-20 09:19:34 -0800

    add text to speech

--- image_server/app.js
+++ image_server/app.js
@@ -6,15 +6,18 @@ const fileType = require('file-type')
 const fs = require('fs')
 const app = express()
 const router = express.Router()
+const util = require('util');
 
 const port     = process.env.PORT || 8080;
 
 const requestOcr = require('request')
+const bodyParser = require("body-parser");
 //const ocrurl = "http://localhost:5000/v1/ocr";
 //const imageurl = "http://localhost:" + port
 //
-var outputJson = ''
-var labels = []
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const upload = multer({
     dest:'images/', 
@@ -39,6 +42,43 @@ router.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
     //res.json({output: 'Welcome to OCR API'})
 })
+
+
+//For speech 
+
+async function getSpeech(text, outputFile) {
+    const textToSpeech = require('@google-cloud/text-to-speech');
+    
+    
+
+    const client = new textToSpeech.TextToSpeechClient();
+
+    /**
+     * TODO(developer): Uncomment the following lines before running the sample.
+     */
+    //const text = 'Hello';
+    var outputPath = 'voices/' + outputFile;
+    console.log(outputPath)
+    //var file = fs.createWriteStream(outputFile);
+
+    const request = {
+      input: {text: text},
+      voice: {languageCode: 'en-US', ssmlGender: 'FEMALE'},
+      audioConfig: {audioEncoding: 'MP3'},
+    };
+    return await client.synthesizeSpeech(request).audioContent;
+
+    //const writeFile = util.promisify(fs.writeFile);
+    //await writeFile(outputPath, response.audioContent, 'binary');
+    //console.log(`Audio content written to file: ${outputPath}`);
+    //
+    //console.log("Audio:----------------" )
+    //console.log(response.audioContent);
+    //const audiobase64 = Buffer.from(response.audioContent, 'binary').toString('base64');
+    //return audiobase64;
+}
+
+
 
 
 router.post('/images/upload2', (req, res) => {
@@ -222,6 +262,8 @@ router.post('/images/upload3', (req, res) => {
 
 router.post('/images/upload', (req, res) => {
 
+    var outputJson = '';
+    var labels = [];
     try{
         upload(req, res, function (err) {
 
@@ -235,9 +277,11 @@ router.post('/images/upload', (req, res) => {
 
                 // get the uploaded image path
                 //let path = `/images/${req.file.filename}`
+                var voicename = `${req.file.filename}`
                 try{
                 // get the uploaded image path
                     var path = `images/${req.file.filename}`
+
                     console.log(path)
                 }catch(error){
                     res.status(400).json({output: error.message})
@@ -258,8 +302,8 @@ router.post('/images/upload', (req, res) => {
                       .then(results => {
                         const fullTextAnnotation = results[0].fullTextAnnotation;
                         console.log('fullTextAnnotation:');
-                        if(typeof(fullTextAnnotation) == 'undefined'){
-                            //outputJson = 'Unrecognized Text';
+                        if(typeof(fullTextAnnotation) == 'undefined' || !fullTextAnnotation || !(fullTextAnnotation.text) ){
+                            outputJson = 'Unrecognized Text';
                             console.log("Unrecognized Text");
                         }else{
                             //detections.forEach(text => console.log(text.description));
@@ -293,10 +337,12 @@ router.post('/images/upload', (req, res) => {
                                 labels = labels.slice(0, 5);
                             }
 
+                            //var voicepath = 'voices/'+ voicename +'.mp3';
                             //res.status(200).json({labels: labels.toString()});
                             res.status(200).json({
                                 output: outputJson,
-                                labels: labels.toString()});
+                                labels: labels.toString()
+                                })
                             res.end();
 
                         })
@@ -333,6 +379,91 @@ router.post('/images/upload', (req, res) => {
     }
 })
 
+router.get('/voices', (req, res) => {
+
+    try{
+        var voicetext = req.query.voicetext;
+        console.log(voicetext)
+        const textToSpeech = require('@google-cloud/text-to-speech');
+        const client = new textToSpeech.TextToSpeechClient();
+        
+        //var file = fs.createWriteStream(outputFile);
+
+        const request = {
+          input: {text: voicetext},
+          voice: {languageCode: 'en-US', ssmlGender: 'FEMALE'},
+          audioConfig: {audioEncoding: 'MP3'},
+        };
+
+        var audiodata = '';
+        client
+          .synthesizeSpeech(request)
+          .then(response => {
+            //const audioContent = response[0].audioContent;
+            console.log('Audio: ---------');
+            //if(typeof(audioContent) !== "undefined" && audioContent !==''){
+            const audiobase64 = Buffer.from(response[0].audioContent, 'binary').toString('base64'); 
+            
+            res.status(200).json({
+                audioContent: audiobase64
+                })
+            res.end();
+
+        })
+        .catch(err => {
+        console.error('ERROR:', err);
+        //throw err;
+        res.status(400).json({audioContent: ""})
+        res.end()
+        });
+    }catch(err){
+        Console.error('ERROR:', err);
+        //throw err;
+        res.status(400).json({audioContent: ""})
+        res.end()
+    }
+})
+
+
+router.post('/voices/upload', (req, res) => {
+
+    var voicetext = req.body.voicetext//`${req.body.voicetext}`;
+    console.log(voicetext)
+    const textToSpeech = require('@google-cloud/text-to-speech');
+    const client = new textToSpeech.TextToSpeechClient();
+    
+    //var file = fs.createWriteStream(outputFile);
+
+    const request = {
+      input: {text: voicetext},
+      voice: {languageCode: 'en-US', ssmlGender: 'FEMALE'},
+      audioConfig: {audioEncoding: 'MP3'},
+    };
+
+    var audiodata = '';
+    client
+      .synthesizeSpeech(request)
+      .then(response => {
+        //const audioContent = response[0].audioContent;
+        console.log('Audio: ---------');
+        //if(typeof(audioContent) !== "undefined" && audioContent !==''){
+        const audiobase64 = Buffer.from(response[0].audioContent, 'binary').toString('base64'); 
+        
+        res.status(200).json({
+            audioContent: audiobase64
+            })
+        res.end();
+
+    })
+    .catch(err => {
+    console.error('ERROR:', err);
+    //throw err;
+    res.status(400).json({audioContent: ""})
+    res.end()
+    });
+
+})
+
 router.get('/images/:imagename', (req, res) => {
 
     let imagename = req.params.imagename
@@ -344,13 +475,28 @@ router.get('/images/:imagename', (req, res) => {
     res.end(image, 'binary')
 })
 
+router.get('/voices/:voicename', (req, res) => {
+    let voicename = req.params.voicename
+    let voicepath = __dirname + "/voices/" + voicename
+    let voice = fs.statSync(voicepath)
+    //let mime = fileType(voice).
+    res.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': voice.size
+    });
+    const readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to util.pump()
+    util.pump(readStream, res);
+})
+
 app.use('/', router)
+
 
 app.use((err, req, res, next) => {
 
     if (err.code == 'ENOENT') {
         
-        res.status(404).json({output: 'Image Not Found !'})
+        res.status(404).json({output: ''})
 
     } else {
 
